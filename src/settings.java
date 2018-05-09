0f6bbd35fee214af5a37b252faefb858fa587213
joeyHargenrader <39094417+joeyHargenrader@users.noreply.github.com>
2018-05-08 22:01:44 -0400

    Update settings.java

--- src/settings.java
+++ src/settings.java
@@ -1,26 +1,25 @@
 import java.awt.*;
 import javax.swing.*;
-
-public class settings {
-  	  public Settings() {
-		    super("Settings");
-		    this.setLayout(new GridLayout(4, 2));
-		    this.setBackground(Color.WHITE);
-		    this.add(new JLabel("Background Color"));
-	  	  String[] colorList = {"Black", "Blue", "Red", "White", "Yellow"};
-		    JComboBox backgrColor = new JComboBox(colorList);
-		    this.add(backgrColor);
-		    this.add(new JLabel("Snake Body Color"));
-		    JComboBox snakeBodyColor = new JComboBox(colorList);
-		    this.add(snakeBodyColor);
-		    this.add(new JLabel("Snake Head Color"));
-		    JComboBox snakeHeadColor = new JComboBox(colorList);
-		    this.add(snakeHeadColor);
-		    this.add(new JLabel("Speed"));
-		    JSlider speed = new JSlider(1, 8, 1);
-		    speed.setPaintTicks(true);
-		    speed.setMajorTickSpacing(1);
-		    this.add(speed);
-		    this.setVisible(true);
-      }
+public class Settings extends JLabel {
+	public settings() {
+		super("Settings");
+		this.setLayout(new GridLayout(4, 2));
+		this.setBackground(Color.WHITE);
+		this.add(new JLabel("Background Color"));
+		String[] colorList = {"Black", "Blue", "Red", "White", "Yellow"};
+		JComboBox backgrColor = new JComboBox(colorList);
+		this.add(backgrColor);
+		this.add(new JLabel("Snake Body Color"));
+		JComboBox snakeBodyColor = new JComboBox(colorList);
+		this.add(snakeBodyColor);
+		this.add(new JLabel("Snake Head Color"));
+		JComboBox snakeHeadColor = new JComboBox(colorList);
+		this.add(snakeHeadColor);
+		this.add(new JLabel("Speed"));
+		JSlider speed = new JSlider(1, 8, 1);
+		speed.setPaintTicks(true);
+		speed.setMajorTickSpacing(1);
+		this.add(speed);
+		this.setVisible(true);
+	}
 }
